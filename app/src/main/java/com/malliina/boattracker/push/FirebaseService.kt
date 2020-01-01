b3e04348ac651f280990bbea6c492f6c4f531d2f
Michael Skogberg <malliina123@gmail.com>
2020-01-01 18:13:30 +0200

    Up deps

--- app/src/main/java/com/malliina/boattracker/push/FirebaseService.kt
+++ app/src/main/java/com/malliina/boattracker/push/FirebaseService.kt
@@ -14,34 +14,36 @@ import com.malliina.boattracker.R
 import com.malliina.boattracker.ui.map.MapActivity
 import timber.log.Timber
 
-class FirebaseService: FirebaseMessagingService() {
+class FirebaseService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         Timber.i("Got token $token")
         PushService.getInstance(application).update(PushToken(token))
     }
 
-    override fun onMessageReceived(message: RemoteMessage?) {
+    override fun onMessageReceived(message: RemoteMessage) {
         Timber.i("Received remote message.")
-        message?.let { msg ->
-            val title = msg.data["title"] ?: "Title here"
-            val text = msg.data["message"] ?: "Message here"
-            val intent = Intent(this, MapActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            val channelId = "fcm_default_channel"
-            val pending = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
-            val builder = NotificationCompat.Builder(this, channelId)
-                .setSmallIcon(R.drawable.ic_trophy)
-                .setContentTitle(title)
-                .setContentText(text)
-                .setContentIntent(pending)
-                .setAutoCancel(true)
-            val manager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val channel = NotificationChannel(channelId, "Channel title", NotificationManager.IMPORTANCE_DEFAULT)
-                manager.createNotificationChannel(channel)
-            }
-            manager.notify(0, builder.build())
+        val title = message.data["title"] ?: "Title here"
+        val text = message.data["message"] ?: "Message here"
+        val intent = Intent(this, MapActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val channelId = "fcm_default_channel"
+        val pending = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_trophy)
+            .setContentTitle(title)
+            .setContentText(text)
+            .setContentIntent(pending)
+            .setAutoCancel(true)
+        val manager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Channel title",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            manager.createNotificationChannel(channel)
         }
-
+        manager.notify(0, builder.build())
     }
 }
